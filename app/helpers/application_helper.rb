83de01bc2f0c933c43f29d2de9d1f8b2ad7a0147
“JohnRRRx” <tom043838@gmail.com>
2024-10-12 15:21:12 +0900

    FlashMessage_added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when :notice then "bg-emerald-300/50"
+    when :error then "bg-red-400/40"
+    else "bg-gray-500/60"
+    end
+  end
 end
