c6c2d1a1f27853154527573bbbbd6092df9f1cac
“JohnRRRx” <tom043838@gmail.com>
2024-10-12 10:53:51 +0900

    tailwind_config_fixed3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 module.exports = {
-    content: [
-      './app/views/**/*.{erb,haml,html,slim}',
-      './app/helpers/**/*.rb',
-      './app/assets/stylesheets/**/*.css',
-      './app/javascript/**/*.js',
-      './config/initializers/simple_form.rb',
-      './lib/components/**/*.{erb,haml,html,slim}',
-      './app/components/**/*.{erb,haml,html,slim}'
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ]
+}
