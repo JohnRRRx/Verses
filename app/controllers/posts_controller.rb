31d17e098f1c587e7780e73658f28e22784ce32e
“JohnRRRx” <tom043838@gmail.com>
2024-10-15 09:54:49 +0900

    add:投稿一覧、投稿詳細、画像アップロード機能

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.includes(:user)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: t('defaults.flash_message.created')
+    else
+      flash.now[:error] = t('defaults.flash_message.not_created')
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :photo, :photo_cache)
+  end
+end
+
