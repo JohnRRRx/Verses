301bf32d97b93ca8a3fc5f5897ed41c6341957f8
“JohnRRRx” <tom043838@gmail.com>
2024-10-15 09:54:49 +0900

    add:投稿一覧、投稿詳細、画像アップロード機能

---
+++ db/migrate/20241014123140_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[7.2]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.string :photo, null: false
+      t.string :song_id
+      t.string :song_name
+      t.string :artist_name
+      t.string :album_name
+      t.string :audio
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
