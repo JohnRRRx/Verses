bc4973e4b51838c8d5f46e2a68a02cf87f9fdd8d
“JohnRRRx” <tom043838@gmail.com>
2024-10-16 11:19:49 +0900

    add: edit&delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+require 'itunes_search_api'
+
   def index
     @posts = Post.includes(:user)
   end
@@ -19,6 +21,38 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: t('defaults.flash_message.updated')
+    else
+      flash.now[:error] = t('defaults.flash_message.not_updated')
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    post = current_user.posts.find(params[:id])
+    post.destroy!
+    redirect_to posts_path, notice: t('defaults.flash_message.deleted'), status: :see_other
+  end
+
+  def search
+    @searchs = ITunesSearchAPI.search(
+      :term => params[:term],
+      :country => 'jp',
+      :media => 'music',
+      :lang => 'ja_jp',
+      :limit => '4'
+      ).each do |item|
+        p item
+    end
   end
 
   private
