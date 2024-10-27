031c412fbb92cae2a01059666636fe998992c144
“JohnRRRx” <tom043838@gmail.com>
2024-10-28 08:43:44 +0900

    fix: Update,FlashMessage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true
-  validates :photo, presence: true
+  validates :photo, presence: { message: "を選択してください" }, on: :create
   validates :song_id, presence: { message: "を選択してください" }
   belongs_to :user
   has_many :likes, dependent: :destroy
