6f69e01bbd5cc7c851430d026e65904204697dc3
“JohnRRRx” <tom043838@gmail.com>
2024-11-02 05:25:21 +0900

    fix: post_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-Rails.application.routes.draw do
+# frozen_string_literal: true
 
-  root "static_pages#top"
+Rails.application.routes.draw do
+  root 'static_pages#top'
   resources :users, only: %i[new create]
   resources :posts, only: %i[index new create show edit update destroy] do
     collection do
@@ -13,8 +14,7 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  get "up" => "rails/health#show", as: :rails_health_check
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
+  get 'up' => 'rails/health#show', as: :rails_health_check
+  get 'service-worker' => 'rails/pwa#service_worker', as: :pwa_service_worker
+  get 'manifest' => 'rails/pwa#manifest', as: :pwa_manifest
 end
