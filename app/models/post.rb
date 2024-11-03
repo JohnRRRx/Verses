523df1e2d64ff34f39c889b80aeb76a40571f34d
“JohnRRRx” <tom043838@gmail.com>
2024-11-03 23:44:32 +0900

    fix: post&login_layout

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: { message: 'を選択してください' }
   validates :photo, presence: { message: 'を選択してください' }, on: :create
   validates :song_id, presence: { message: 'を選択してください' }
   belongs_to :user
