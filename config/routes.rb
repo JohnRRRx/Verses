20f01ab9b19b5311b9b6c4b24d99ababa2c4f58f
“JohnRRRx” <tom043838@gmail.com>
2024-11-08 15:53:37 +0900

    add: google_oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "oauths/oauth"
+  get "oauths/callback"
   root 'static_pages#top'
   resources :users, only: %i[new create]
   resources :posts, only: %i[index new create show edit update destroy] do
@@ -13,8 +15,11 @@ Rails.application.routes.draw do
   get 'music/search', to: 'musics#search'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
+  post "oauth/callback" => "oauths#callback"
   delete 'logout', to: 'user_sessions#destroy'
   get 'up' => 'rails/health#show', as: :rails_health_check
   get 'service-worker' => 'rails/pwa#service_worker', as: :pwa_service_worker
   get 'manifest' => 'rails/pwa#manifest', as: :pwa_manifest
+  get "oauth/callback" => "oauths#callback"
+  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,16 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_04_041117) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "authentications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
+    t.index ["user_id"], name: "index_authentications_on_user_id"
+  end
+
   create_table "likes", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "post_id"
