6cd3f13ee648673f867579b38a45338678544d85
“JohnRRRx” <tom043838@gmail.com>
2024-11-14 12:37:35 +0900

    fix: production_domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'https://slimy-ilsa-nonamejusttest-74953f89.koyeb.app/',
+    domain:               'slimy-ilsa-nonamejusttest-74953f89.koyeb.app',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
