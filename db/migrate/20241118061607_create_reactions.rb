cb28ea3fbb64751d5d205b8fd8944314c6405511
“JohnRRRx” <tom043838@gmail.com>
2024-11-25 20:52:44 +0900

    add: extra_db

---
+++ db/migrate/20241118061607_create_reactions.rb
@@ -0,0 +1,14 @@
+class CreateEmojis < ActiveRecord::Migration[7.2]
+    def change
+      create_table :emojis do |t|
+  
+        t.references :post, null: false, foreign_key: true
+        t.references :user, null: false, foreign_key: true
+        t.string :name, null: false
+  
+        t.timestamps
+      end
+  
+      add_index :emojis, [:user_id, :post_id, :type], unique: true
+    end
+  end
