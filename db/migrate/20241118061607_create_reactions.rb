cacff55da85f2111747d0d86f9d8c5ffe2e075d8
“JohnRRRx” <tom043838@gmail.com>
2024-11-25 21:15:52 +0900

    fix: extra_db2

--- db/migrate/20241118061607_create_reactions.rb
+++ db/migrate/20241118061607_create_reactions.rb
@@ -1,6 +1,6 @@
 class CreateReactions < ActiveRecord::Migration[7.2]
     def change
-      create_table :emojis do |t|
+      create_table :reactions do |t|
   
         t.references :post, null: false, foreign_key: true
         t.references :user, null: false, foreign_key: true
