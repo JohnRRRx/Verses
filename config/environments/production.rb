d12acfd32a70cbd460dba247d8948e8cf306d6ee
“JohnRRRx” <tom043838@gmail.com>
2024-11-28 19:47:12 +0900

    test_clear

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'https://verses-take.fly.dev',
+    domain:               'verses-take.fly.dev',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
