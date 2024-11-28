ed5d0acf0e3d19e8034b7ec77731d72f90d4e19f
“JohnRRRx” <tom043838@gmail.com>
2024-11-28 13:38:38 +0900

    fix: js_import

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,3 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin '@rails/actioncable/src', to: '@rails--actioncable--src.js' # @7.2.102
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
-pin_all_from 'app/javascript', under: 'spotify_search'
--- public/assets/application-ee7b318cabcd2b4af8adacc62f07417cc389449838eeda1873ca5f441219909b.js
+++ public/assets/application-ee7b318cabcd2b4af8adacc62f07417cc389449838eeda1873ca5f441219909b.js
@@ -1,4 +1,4 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import './controllers/spotify_search'
-import "./flash.js"
+import "./flash.js";
