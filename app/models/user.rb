30af48e0bc22aa4984372face9de1a8d571b1744
“JohnRRRx” <tom043838@gmail.com>
2024-12-10 16:57:06 +0900

    fix: error_message_order

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
   has_many :authentications, :dependent => :destroy
   has_many :reactions, dependent: :destroy
   accepts_nested_attributes_for :authentications
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :name, presence: true, length: { maximum: 255 }
-  validates :email, presence: true, uniqueness: true
   validates :reset_password_token, uniqueness: true, allow_nil: true
   mount_uploader :icon, IconUploader
 
