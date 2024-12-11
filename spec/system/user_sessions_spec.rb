a352568e7cbf7d075980cf6451d94ffa7131278b
“JohnRRRx” <tom043838@gmail.com>
2024-12-12 06:33:18 +0900

    add: user_sessions_system_test

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,9 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe "UserSessions", type: :system do
-  before do
-    driven_by(:rack_test)
+RSpec.describe 'UserSessions', type: :system do
+  let(:user) { create(:user) }
+
+  describe 'ログイン前' do
+    context 'フォームの入力値正常' do
+      it 'ログイン成功' do
+        visit login_path
+        fill_in 'メールアドレス', with: user.email
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログイン'
+        expect_message("ログインしました")
+        expect(current_path).to eq root_path
+      end
+    end
+
+    context 'フォーム未入力' do
+      it 'ログイン失敗' do
+        visit login_path
+        fill_in 'メールアドレス', with: ''
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログイン'
+        expect_message("ログインに失敗しました")
+        expect(current_path).to eq login_path
+      end
+    end
+
+    context '間違ったメールアドレスでログイン' do
+      it 'ログイン失敗' do
+        visit login_path
+        fill_in 'メールアドレス', with: 'wrong@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログイン'
+        expect_message("ログインに失敗しました")
+        expect(current_path).to eq login_path
+      end
+    end
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ログイン後' do
+    context 'ログアウトボタンをクリック' do
+      it 'ログアウト成功' do
+        login_as(user)
+        navibar_click
+        click_link 'ログアウト'
+        expect_message("ログアウトしました")
+        expect(current_path).to eq root_path
+      end
+    end
+  end
 end
