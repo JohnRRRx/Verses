5452a4da60983fad1578da09624ea64129c53ff7
“JohnRRRx” <tom043838@gmail.com>
2024-12-13 00:48:40 +0900

    fix: password_reset_system_test

--- spec/system/password_resets_spec.rb
+++ spec/system/password_resets_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'パスワードリセット', type: :system do
+RSpec.describe 'Password_reset', type: :system do
   let(:user) { create(:user) }
 
   describe '表示確認' do
@@ -15,24 +15,25 @@ RSpec.describe 'パスワードリセット', type: :system do
       it '正しい要素が表示されていること' do
         user.generate_reset_password_token!
         visit edit_password_reset_path(user.reset_password_token)
-        puts page.body
         expect(page).to have_selector('.title', text: 'パスワードリセット')
         expect(page).to have_selector('label[for="user_email"]', text: 'メールアドレス')
       end
     end
   end
 
-  it 'パスワード変更可能' do
-    visit new_password_reset_path
-    fill_in 'メールアドレス', with: user.email
-    click_button '送信'
-    expect_text('パスワードリセットメールを送信しました')
-    visit edit_password_reset_path(user.reload.reset_password_token)
-    fill_in 'パスワード', with: '123456789'
-    fill_in 'パスワード確認', with: '123456789'
-    click_button '送信'
-    Capybara.assert_current_path("/login", ignore_query: true)
-    expect(current_path).to eq(login_path)
-    expect_text('パスワードを変更しました')
+  describe 'パスワード変更' do
+    it 'パスワード変更可能' do
+      visit new_password_reset_path
+      fill_in 'メールアドレス', with: user.email
+      click_button '送信'
+      expect_text('パスワードリセットメールを送信しました')
+      visit edit_password_reset_path(user.reload.reset_password_token)
+      fill_in 'パスワード', with: '123456789'
+      fill_in 'パスワード確認', with: '123456789'
+      click_button '送信'
+      Capybara.assert_current_path("/login", ignore_query: true)
+      expect(current_path).to eq(login_path)
+      expect_text('パスワードを変更しました')
+    end
   end
 end
