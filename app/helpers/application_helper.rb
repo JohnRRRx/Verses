b73a39993d9c8175dafd426d1a1d473934ec636d
“JohnRRRx” <tom043838@gmail.com>
2024-12-16 07:38:31 +0900

    add: static_OGP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,31 @@ module ApplicationHelper
     else 'bg-gray-500/60'
     end
   end
+
+  def default_meta_tags
+    {
+      site: 'Verses',
+      title: '文字、写真、音楽をまとめて投稿できるアプリです',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'あの瞬間、何の曲を思い出した？',
+      keywords: '音楽',
+      canonical: Rails.env.production? ? 'https://verses-take.fly.dev/' : 'http://localhost:3000/',
+      separator: '|',
+      og:{
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: Rails.env.production? ? 'https://verses-take.fly.dev/' : 'http://localhost:3000/',
+        image: image_url('ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@JohnRRRxx',
+        image: image_url('ogp.png')
+      }
+    }
+  end
 end
